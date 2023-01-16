5080f25e2c6536a78dfe6a9984295d56e5399f1b
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-01-16 12:08:27 +0300

    replaced object literal with object constructors,added delete functionality

--- script-lib.js
+++ script-lib.js
@@ -1,41 +1,49 @@
 let library =[];
+//creating variables
 const cardContainer = document.querySelector("#card-container")
 const addBtn = document.querySelector(".add")
 const closeBtn = document.querySelector("#close-btn")
 const form = document.querySelector("form")
 const formContainer = document.querySelector("#form-container")
-const addBook = (ev)=>{
-    ev.preventDefault();
-    let book ={
-        title: document.querySelector("#title").value,
-        author:document.querySelector("#author").value,
-        pages:document.querySelector("#pages").value
+//book constructor
+class Book {
+    constructor(author, title, pages) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
     }
-    library.push(book)
-    document.querySelector("form").reset();
-    //or document.forms[0].reset(); to clear the form for the next entries
+}
+//new adding book function
+const newAddBook =(e)=>{
+    e.preventDefault();
+    const bookTitle =document.querySelector("#title").value
+    const bookAuthor=document.querySelector("#author").value
+    const bookPages=document.querySelector("#pages").value
+    let newBook = new Book(bookAuthor,bookTitle,bookPages);
+    library.push(newBook);
+    document.querySelector("form").reset(); //clear for new entries
     console.warn("added",{library})
-    
-    //let pre = document.querySelector("#msg ");
-    //pre.textContent = '\n' + JSON.stringify(library,'\t',2)
-     const myCard=document.createElement("div")
+    createCardElements(newBook)
+}
+//new function for making card elements
+function createCardElements(newBook){ //add param ? + at 24?
+    const myCard=document.createElement("div")
      myCard.classList.add("card")
      const cardCloseBtn = document.createElement("button")
-     
      const statusBtn = document.createElement("button")
-
-     //myCard.innerText = book.title +"\n" + book.author +"\n" + book.pages
      const titleText = document.createElement("h2")
      const authorText = document.createElement("h2")
      const pagesText= document.createElement("h2")
+     //adding relevant styling classes to elements
      titleText.classList.add("card-text")
      authorText.classList.add("card-text")
      pagesText.classList.add("card-text")
      cardCloseBtn.classList.add("card-close")
      statusBtn.classList.add("status")
-     titleText.innerText = "Title : "+ book.title
-     authorText.innerText = "Author :   "+ book.author
-     pagesText.innerText = "Pages :  "+book.pages
+     //adding text to elements
+     titleText.innerText = "Title : "+ newBook.title
+     authorText.innerText = "Author :   "+ newBook.author
+     pagesText.innerText = "Pages :  "+newBook.pages
      cardCloseBtn.innerHTML = "REMOVE"
      statusBtn.innerHTML = "HAVE READ"
      myCard.appendChild(titleText)
@@ -44,9 +52,15 @@ const addBook = (ev)=>{
      myCard.appendChild(cardCloseBtn)
      myCard.appendChild(statusBtn)
      cardContainer.appendChild(myCard)
-     cardCloseBtn.addEventListener("click",()=>{
-        myCard.remove()
-    })
+
+     cardCloseBtn.addEventListener("click",(selectedBook)=>{
+        console.log("to be removed")
+        library.splice(selectedBook,1) //is display vs current array contents  okay?
+       myCard.remove()
+        
+     })
+
+
     function toggletext(){
         if(statusBtn.innerHTML==="HAVE READ"){
             statusBtn.innerHTML = "NOT READ"
@@ -54,13 +68,24 @@ const addBook = (ev)=>{
         }
         statusBtn.addEventListener("click",toggletext)
 }
-document.addEventListener('DOMContentLoaded',()=>{
-    document.getElementById("btn").addEventListener("click",addBook)
-})
 
+//function to add book added to button
+document.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById("btn").addEventListener("click",newAddBook)
+})
+//removes form
 closeBtn.addEventListener("click",()=>{
+    
     form.remove();
 })
+//adds form to container
 addBtn.addEventListener("click",()=>{
     formContainer.appendChild(form)
 })
+//default view
+window.addEventListener("load", (event) => {
+    form.remove();
+  });
+  
+
+
